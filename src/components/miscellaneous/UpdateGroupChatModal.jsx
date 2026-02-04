b25b492609d7683968c77c71afc68c6107955a92
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-02-05 02:22:30 +0530

    prod changes9

--- src/components/miscellaneous/UpdateGroupChatModal.jsx
+++ src/components/miscellaneous/UpdateGroupChatModal.jsx
@@ -16,7 +16,7 @@ import {
   IconButton,
   Spinner,
 } from "@chakra-ui/react";
-import API from "../config/api";
+import API from "../../config/api";
 import { useState } from "react";
 import { ChatState } from "../../Context/ChatProvider";
 import UserBadgeItem from "../userAvatar/UserBadgeItem";
