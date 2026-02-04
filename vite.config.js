0e26c79875d595a4a37ec42a8ea91be270f7e333
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-02-05 00:35:32 +0530

    prod changes1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000"
+      // "/api": "http://localhost:5000"
+      "/api": "https://buddychat-app-backend.onrender.com"
     }
   }
 })
